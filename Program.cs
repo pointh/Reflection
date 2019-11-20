2ff31a1be3b57fb867eba604517c611cfbc12be0
pavel.svec <pavel.svec@NTB-SVEC>
2019-11-20 13:35:52 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        public static void NedelaNic()
+        {
+            Type t = typeof(Program);
+            // vloží jen public metody!
+            MemberInfo[] mi = t.GetMethods();
+            foreach (var m in mi)
+                Console.WriteLine(m.Name);
+        }
+
+        static void Main(string[] args)
+        {
+            NedelaNic();
+        }
+    }
+}
