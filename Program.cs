fc2e1fa6d0045f354dc3ab346879493905322ce5
pavel.svec <pavel.svec@NTB-SVEC>
2019-11-20 14:10:49 +0100

    MemberBase, MethodInfo a MemberInfo

--- Program.cs
+++ Program.cs
@@ -11,11 +11,24 @@ namespace Reflection
     {
         public static void NedelaNic()
         {
+            int i = 1;
+
             Type t = typeof(Program);
             // vloží jen public metody!
-            MemberInfo[] mi = t.GetMethods();
-            foreach (var m in mi)
-                Console.WriteLine(m.Name);
+            MemberInfo[] mmbi = t.GetMethods();
+            Console.WriteLine("Public methods of this class");
+            foreach (var item in mmbi)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine();
+
+            MethodInfo mthi = t.GetMethod(MethodBase.GetCurrentMethod().Name);
+
+            Console.WriteLine("\nThis method");
+            Console.WriteLine("Method name: " + mthi.Name);
+            Console.WriteLine("Local variables: " + mthi.GetMethodBody().LocalVariables[0]);
+            Console.WriteLine("Return type: " + mthi.ReturnType);
         }
 
         static void Main(string[] args)
